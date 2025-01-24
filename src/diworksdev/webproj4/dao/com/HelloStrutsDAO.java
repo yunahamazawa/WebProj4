f203ab1f895fc7c710cdbbf39852790b67a3aeeb
yunahamazawa <yunahamazawa821@gmail.com>
2025-01-24 22:41:01 +0900

    HelloStrutsDAO.java作成

---
+++ src/diworksdev/webproj4/dao/com/HelloStrutsDAO.java
@@ -0,0 +1,35 @@
+package diworksdev.webproj4.dao.com;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import diworksdev.webproj4.util.DBConnector;
+public class HelloStrutsDAO {
+
+
+	public boolean select() {
+		boolean ret=false;
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+		String sql = "select * from users";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				ret=true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ret;
+	}
+
+}
